bba4f2ffe9b046f9d1eb1e620120bd8d05d517e5
jparajeles <josepabloparaj@gmail.com>
2015-10-19 09:28:10 -0600

    Clean up

--- js/Directives/DefaultImageDirective.js
+++ js/Directives/DefaultImageDirective.js
@@ -1,12 +1,15 @@
-app.directive('checkImage', function($http) {
+app.directive('errSrc', function () {
     return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            attrs.$observe('ngSrc', function(ngSrc) {
-                $http.get(ngSrc)
-                .error(function(){
-                    element.attr('src', '../../Img/defaultAtleta.png'); // set default image
-                });
+        link: function (scope, element, attrs) {
+            scope.$watch(function () {
+                return attrs.ngSrc;
+            }, function (value) {
+                if (!value) {
+                    element.attr('src', attrs.errSrc);
+                }
+            });
+            element.bind('error', function () {
+                element.attr('src', attrs.errSrc);
             });
         }
     };
